0652428a792322ee1ec0e55b9db75d7a4b2e7562
NoahNMorton <noahmorton98@gmail.com>
2017-05-03 14:30:03 -0500

    Create panel

--- src/gamewindow/SneakPanel.java
+++ src/gamewindow/SneakPanel.java
@@ -1,5 +1,6 @@
 package gamewindow;
 
+import java.awt.event.*;
 import javax.swing.JPanel;
 
 /**
@@ -11,6 +12,64 @@ import javax.swing.JPanel;
  *
  * Part of project: ScrollingGame
  */
-public class SneakPanel extends JPanel {
+public class SneakPanel extends JPanel implements MouseListener, MouseMotionListener, KeyListener {
+
+    public SneakPanel() {
+        setSize(500, 500); //todo placeholder size
+
+        addMouseListener(this);
+        addMouseMotionListener(this);
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
