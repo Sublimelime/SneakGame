b205295eccea57cd351966c78d853a806d1c4c30
Noah Morton <noahmorton98@gmail.com>
2017-05-11 18:51:05 -0500

    Added Enemy class, removed enemy move check

---
+++ src/gamelogic/Enemy.java
@@ -0,0 +1,18 @@
+package gamelogic;
+
+/**
+ * A class for the basic enemy.
+ *
+ * @author Noah Morton
+ *
+ * Date created: May 11, 2017
+ *
+ * Part of project: SneakGame
+ */
+public class Enemy extends Player {
+
+    public Enemy(int x, int y, SneakGame game) {
+        super(x, y, game);
+    }
+
+}
